33ff0548cda465a7d7f28f0a78cc083bf11c3795
郭永坤 <1272135487@qq.com>
2022-06-01 23:50:49 +0800

    gyk612350

---
+++ guo.js/index.js
@@ -0,0 +1,20 @@
+var lone=document.querySelector('.lone');
+lone.addEventListener('click',function(){
+     window.location.assign("http://127.0.0.1:5501/find-house/guo/beijing.html")
+})
+
+var lsecond=document.querySelector('.lsecond')
+lsecond.ontouchstart=function(){
+    window.location.assign("http://127.0.0.1:5501/find-house/guo/detail.html")
+}
+var lthird=document.querySelector('.lthird')
+lthird.ontouchstart=function(){
+    window.location.assign("http://127.0.0.1:5501/find-house/guo/housemessage.html")
+}
+var lfourth=document.querySelector('.lfourth')
+lfourth.ontouchstart=function(){
+    window.location.assign("http://127.0.0.1:5501/find-house/guo/like.html")
+}
+
+
+
---
+++ guo.js/touchmove.js
@@ -0,0 +1,4 @@
+document.ontouchmove=function(){
+    console.log(1);
+    window.location.assign("http://127.0.0.1:5501/find-house/index.html")
+}
