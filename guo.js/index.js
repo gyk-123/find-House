d9e8c110f7057ea92c362cba41ebd1cab1ce23e2
郭永坤 <1272135487@qq.com>
2022-06-02 14:38:11 +0800

    修改链接

--- guo.js/index.js
+++ guo.js/index.js
@@ -1,20 +1,21 @@
 var lone=document.querySelector('.lone');
 lone.addEventListener('click',function(){
-     window.location.assign("http://127.0.0.1:5501/find-house/guo/beijing.html")
+     window.location.assign("./../guo/beijing.html")
 })
 
 var lsecond=document.querySelector('.lsecond')
 lsecond.ontouchstart=function(){
-    window.location.assign("http://127.0.0.1:5501/find-house/guo/detail.html")
+    window.location.assign("./../guo/detail.html")
 }
 var lthird=document.querySelector('.lthird')
 lthird.ontouchstart=function(){
-    window.location.assign("http://127.0.0.1:5501/find-house/guo/housemessage.html")
+    window.location.assign("./../guo/housemessage.html")
 }
 var lfourth=document.querySelector('.lfourth')
 lfourth.ontouchstart=function(){
-    window.location.assign("http://127.0.0.1:5501/find-house/guo/like.html")
+    window.location.assign("./../guo/like.html")
 }
+
 
 
 
--- guo.js/touchmove.js
+++ guo.js/touchmove.js
@@ -1,4 +1,4 @@
 document.ontouchmove=function(){
     console.log(1);
-    window.location.assign("http://127.0.0.1:5501/find-house/index.html")
+    window.location.assign("./../index.html")
 }
